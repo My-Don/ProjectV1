02395f5e9422ae25c4ae0af4108806ac9ed1abb1
My-Don <165189533+My-Don@users.noreply.github.com>
2025-12-23 13:10:27 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat ignition deploy ./ignition/modules/Lock.js
+```
+
+/**        monadtestnet             */
+// usdt deployed to:                           0x8f97cd236fA90b66DdFC5410Dec8eFF0df527F2b
+// weth deployed to:                           0x5e6031572A58f02cd73b3bB5523365D77D1Bb1F4
+// UniswapV2Factory deployed to:               0x15e51143e4F963f719cEc4a99a951bA9C63860c5
+// UniswapV2Router02 deployed to:              0x3C1c9AF1C2686804676194ddd85D7C2363DE4690
+
+
+// bkc  deployed to:                           0xe1c02df62Ed0A25D08b90169cAec6319fd8e9c98
+// wbkc deployed to: 
+// DecreasingRewardCalculator  deployed to:    0x2CE745237CFeF71f5E2e334130D041706f0b6f52
+// serverNode  deployed to:                    0x3E058e7c662843f46bf683EBDA17c7009FC8876D
+
---
+++ hardhat.config.js
@@ -0,0 +1,209 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades"); 
+require("dotenv").config();
+
+
+// 配置hardhat accounts参数
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+          viaIR: true
+        }
+      },
+       {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+          viaIR: true
+        }
+      },
+   /*   {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.6.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+       {
+        version: "0.5.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: "0.4.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+        {
+        version: "0.4.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      */
+    ]
+  },
+  networks: {
+    mainnet: {
+      url: process.env.MAINNET_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+       // 增加超时时间，防止网络问题
+      timeout: 120000, // 120秒
+      gasPrice: "auto",
+      verify: {
+        etherscan: {
+          apiKey: process.env.ETHERSCAN_API_KEY,
+        },
+        sourcify: {
+          enabled: true,  // 启用 Sourcify 验证
+          endpoint: "https://repo.sourcify.dev", 
+        }
+      }
+    },
+    bscTestnet: {
+      url: process.env.BSCTEST_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 97
+    },
+    bsc: {
+      url: process.env.BSCMAINNET_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 56
+    },
+    local: {
+      url: process.env.LOCAL_URL || "",
+      accounts: process.env.LOCAL_PRIVATE_KEY !== undefined ? [process.env.LOCAL_PRIVATE_KEY] : []
+    },
+    monadTestnet: {
+      url: process.env.MONAD_TESTNET_RPC_URL,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 10143
+    },
+    monadMainnet: {
+      url: process.env.MONAD_MAINNET_RPC_URL,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 143
+    },
+
+  },
+ etherscan: {
+  enabled: true,
+    apiKey: {
+      monadMainnet: process.env.ETHERSCAN_API_KEY,
+      monadTestnet: process.env.ETHERSCAN_API_KEY,
+      bsc: process.env.BSCSCAN_API_KEY,
+      bscTestnet: process.env.BSCSCAN_API_KEY,
+      sepolia: process.env.ETHERSCAN_API_KEY
+    },
+    customChains: [
+      {
+        network: "monadTestnet",
+        chainId: 10143,
+        urls: {
+          apiURL: "https://api.etherscan.io/v2/api?chainid=10143", 
+          browserURL: "https://testnet.monadscan.com"
+        }
+      },
+       {
+        network: "monadMainnet",
+        chainId: 143,
+        urls: {
+          apiURL: "https://api.etherscan.io/v2/api?chainid=143", 
+          browserURL: "https://monadscan.com"
+        }
+      },
+      {
+        network: "bscTestnet",
+        chainId: 97,
+        urls: {
+          apiURL: "https://api.etherscan.io/v2/api?chainid=97", 
+          browserURL: "https://testnet.bscscan.com"
+        }
+      },
+      {
+        network: "bsc",
+        chainId: 56,
+        urls: {
+          apiURL: "https://api.etherscan.io/v2/api?chainid=56", 
+          browserURL: "https://bscscan.com"
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: true,
+    apiUrl: "https://sourcify-api-monad.blockvision.org/",
+    browserUrl: "https://monadvision.com"
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
